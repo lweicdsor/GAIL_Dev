ec9869b04b09da4d8c217dd072984a0adbdcd56f
Kan Zhang <zhangkan.zhangkan@gmail.com>
2017-07-09 21:42:55 -0500

    clean Papers folder

--- GAIL_Matlab/Papers/Contents.m
+++ GAIL_Matlab/Papers/Contents.m
@@ -3,9 +3,8 @@
 % Folders
 %   ConesNotBallsPaper  - code of examples in Cones Not Balls Paper  
 %   MCQMC2012Paper      - code of examples in MCQMC paper
+%   OldPapers           - directory for archiving work of old papers
 %   Paper_cubLattice_g  - code of examples in cubLattice_g paper
 %   Paper_cubSobol_g    - code of examples in cubSobol_g paper  
 %   Thesis              - code of examples in various theses
 %   TraubPaper          - code of examples in Traub memorial paper
-%   OldPapers           - directory for archiving work of old papers
-%   
