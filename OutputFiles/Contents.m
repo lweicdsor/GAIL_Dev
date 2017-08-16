c8875c3512aafac4e6674ca0be1b36106ed5ff1f
Kan Zhang <zhangkan.zhangkan@gmail.com>
2017-08-15 19:37:59 -0500

    Update contents.m in OutputFiles.

--- OutputFiles/Contents.m
+++ OutputFiles/Contents.m
@@ -1,10 +1,13 @@
 % OUTPUTFILES
 %
 % Folders
+%   ConesPaperOutput      - output files of Cones Paper
 %   MCQMC2012PaperOutput  - output files of MCQMC2012 paper
+%   meanMCBerPaperOutput  - output files of meanMCBer Paper
 %   Paper_cubLattice_g    - output files of cubLattice_g paper
 %   Paper_cubSobol_g      - output files of cubSobol_g paper
-%   Paper_integral_g      - output files of integral_g paper
+%   TraubPaperOutput      - output files of Traub paper
+%   UniFunMinOutput       - output files of UniFunMin paper
 %   WorkoutFunappxOutput  - output files of funappx_g workouts
 %   WorkoutFunminOutput   - output files of funmin_g workouts
 %   WorkoutIntegralOutput - output files of integral_g workouts
